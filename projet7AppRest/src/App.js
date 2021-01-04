81c60e86272895cd0228ded685cdd7d1b8e67062
TonyM-design <tony.mousli@gmail.com>
2021-01-04 11:45:45 +0100

    Update App.js

--- projet7AppRest/src/App.js
+++ projet7AppRest/src/App.js
@@ -51,25 +51,22 @@ function App(props) {
 const userBounds = useSelector(state => state)
   console.log(userBounds)
      
-
+/*
      // hook manage list of visible restaurant
      const [[filterByBoundsRestaurantLists], setfilterByBoundsRestaurantLists] = useState([]); // état initial vide
      useEffect(() => {
-       // création filtre
+       // filter
        if (userBounds.userBounds.bounds !== undefined && userBounds.userBounds.bounds !== null) {
          const ne = userBounds.userBounds.bounds.ne
          const nw = userBounds.userBounds.bounds.nw
          const se = userBounds.userBounds.bounds.se
          const sw = userBounds.userBounds.bounds.sw
-         console.log(restaurantLists)
-         // et si je séparais la partie suivante en fonction  ?
          const newLists = [];
 
          restaurantLists.map((restaurantList, i) => {
            if (restaurantList.lat <= ne.lat && restaurantList.lat >= se.lat){
              if (restaurantList.long <= ne.lng && restaurantList.long >= sw.lng){
                  newLists.push(restaurantList)
-                 console.log("rajout a la liste (push)")
              }
            }
          })
@@ -78,14 +75,14 @@ const userBounds = useSelector(state => state)
      }, [setfilterByBoundsRestaurantLists]);
 
 
-     
+     */
     
 //VALIDE MAIS A TRANSFORMER EN HOOKS 
-/*
+
   const filterRestaurantLists = (userBounds, restaurantLists) => {
     const filteredRestaurantLists = [];
     console.log(userBounds)
-    if (userBounds.userBounds.bounds !== undefined) {
+    if (userBounds.userBounds.bounds !== undefined && userBounds.userBounds.bounds !== null) {
       restaurantLists.map((restaurantList, i) => {
         console.log('test avant condition')
         if (restaurantList.lat <= userBounds.userBounds.bounds.ne.lat && restaurantList.lat >= userBounds.userBounds.bounds.se.lat){
@@ -99,7 +96,7 @@ const userBounds = useSelector(state => state)
     console.log(filteredRestaurantLists)
     return filteredRestaurantLists
   }
-*/
+
 
 
 
@@ -109,8 +106,8 @@ const userBounds = useSelector(state => state)
         {WatchPosition()}
         {(useDisplayModal && <GeolocationModal></GeolocationModal>) || null}
 
-        <Map newPosition={currentPosition} onChange={useStore()}> </Map>
-        <Aside restaurantLists={props.restaurantLists} restaurantListsFiltre={filterByBoundsRestaurantLists} ></Aside>
+        <Map newPosition={currentPosition} onChange ={filterRestaurantLists(userBounds,restaurantLists)}> </Map>
+        <Aside restaurantLists={props.restaurantLists} ></Aside>
       </Row>
     </Container>
   );
